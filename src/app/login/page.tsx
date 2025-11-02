4b45509c288298dd30e5022ec6acb46c860ffcc1
Phương Huy Đặng <dangphuonghuy0709@gmail.com>
2025-11-02 15:19:16 +0000

    feat: Redesign login and signup pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,21 @@ import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <LoginForm />
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <LoginForm />
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <img 
+          src="https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,23 @@
-import { SignupForm } from '@/components/signup-form';
+
+import { SignupForm } from "@/components/signup-form";
 
 export default function SignupPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <SignupForm />
-    </main>
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <SignupForm />
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <img 
+          src="https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
   );
 }
